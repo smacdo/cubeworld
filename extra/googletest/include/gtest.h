2a50de199cb0051fd1c894e3992f845d041b72cf
Scott MacDonald <illynova@gmail.com>
2010-09-14 22:23:46 -0500

    Final updates to refactored build system. Now to see if it still works on linux!

--- extra/googletest/include/gtest.h
+++
@@ -1,1 +0,0 @@
-#include "googletest/googletest.h"
--- extra/googletest/testrunner.cpp
+++
@@ -1,7 +0,0 @@
-#include "googletest/googletest.h"
-
-int main( int argc, char * argv[] )
-{
-    ::testing::InitGoogleTest( &argc, argv );
-    return RUN_ALL_TESTS();
-}
